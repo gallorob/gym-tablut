8b90967bd81e3e8b1259c6f21846daa37f68507c
gallorob <gallotta.roberto@gmail.com>
2020-08-14 12:20:34 +0200

    self-contained render() so colab is happy

--- gym_tablut/envs/tablut_env.py
+++ gym_tablut/envs/tablut_env.py
@@ -1,6 +1,7 @@
+from typing import Any
+
 import gym
 from gym import spaces, logger
-from gym.envs.classic_control import rendering
 from gym.utils import seeding
 
 from gym_tablut.envs._game_engine import *
@@ -123,13 +124,28 @@ class TablutEnv(gym.Env):
 
         :param mode: The rendering mode to use
         """
+        from gym.envs.classic_control import rendering
+
         if self.viewer is None:
             self.viewer = rendering.Viewer(SCREEN_WIDTH, SCREEN_HEIGHT)
-
+            
             # background
-            tiles = make_background_geoms()
-            for tile in tiles:
-                self.viewer.add_geom(tile)
+            background = rendering.Image(ASSETS.get(BACKGROUND), SCREEN_WIDTH, SCREEN_HEIGHT)
+            background.set_color(1., 1., 1.)
+            background.add_attr(rendering.Transform(translation=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
+            self.viewer.add_geom(background)
+            # add the tiles
+            c = 0
+            for i in range(N_ROWS, 0, -1):
+                for j in range(1, N_COLS + 1):
+                    tile = rendering.make_polygon([(i * SQUARE_WIDTH, j * SQUARE_HEIGHT),
+                                                   ((i + 1) * SQUARE_WIDTH, j * SQUARE_HEIGHT),
+                                                   ((i + 1) * SQUARE_WIDTH, (j + 1) * SQUARE_HEIGHT),
+                                                   (i * SQUARE_WIDTH, (j + 1) * SQUARE_HEIGHT)])
+                    r, g, b = BOARD_COLOR_0 if c == 0 else BOARD_COLOR_1
+                    tile.set_color(r, g, b)
+                    c = 1 if c == 0 else 0
+                    self.viewer.add_geom(tile)
             # throne
             throne = rendering.Image(ASSETS.get(THRONE), SQUARE_WIDTH, SQUARE_HEIGHT)
             throne.set_color(1., 1., 1.)
@@ -137,7 +153,19 @@ class TablutEnv(gym.Env):
                                                              (N_ROWS // 2 + 1) * SQUARE_HEIGHT + (SQUARE_HEIGHT / 2))))
             self.viewer.add_geom(throne)
 
-        add_pieces_to_render(self.viewer, self.board)
+        # add_pieces_to_render(self.viewer, self.board)
+        for i in range(self.board.shape[0]):
+            for j in range(self.board.shape[1]):
+                p = self.board[i, j]
+                if p != EMPTY:
+                    r = i + 1
+                    c = j + 1
+                    tile = rendering.Image(ASSETS.get(p), SQUARE_WIDTH, SQUARE_HEIGHT)
+                    tile.set_color(1., 1., 1.)
+                    tile.add_attr(rendering.Transform(translation=(c * SQUARE_WIDTH + (SQUARE_WIDTH / 2),
+                                                                   (self.board.shape[0] - r + 1) * SQUARE_HEIGHT + (
+                                                                           SQUARE_HEIGHT / 2))))
+                    self.viewer.add_onetime(tile)
 
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
@@ -148,52 +176,3 @@ class TablutEnv(gym.Env):
         if self.viewer:
             self.viewer.close()
             self.viewer = None
-
-
-# -- Rendering aid functions --
-
-
-def make_background_geoms():
-    """
-    Create the checkerboard background for the board
-    """
-    geoms = []
-    # add the actual background
-    background = rendering.Image(ASSETS.get(BACKGROUND), SCREEN_WIDTH, SCREEN_HEIGHT)
-    background.set_color(1., 1., 1.)
-    background.add_attr(rendering.Transform(translation=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
-    geoms.append(background)
-    # add the tiles
-    c = 0
-    for i in range(N_ROWS, 0, -1):
-        for j in range(1, N_COLS + 1):
-            tile = rendering.make_polygon([(i * SQUARE_WIDTH, j * SQUARE_HEIGHT),
-                                           ((i + 1) * SQUARE_WIDTH, j * SQUARE_HEIGHT),
-                                           ((i + 1) * SQUARE_WIDTH, (j + 1) * SQUARE_HEIGHT),
-                                           (i * SQUARE_WIDTH, (j + 1) * SQUARE_HEIGHT)])
-            r, g, b = BOARD_COLOR_0 if c == 0 else BOARD_COLOR_1
-            tile.set_color(r, g, b)
-            c = 1 if c == 0 else 0
-            geoms.append(tile)
-    return geoms
-
-
-def add_pieces_to_render(viewer: rendering.Viewer, board: np.array):
-    """
-    Add the board's pieces to the renderer
-
-    :param viewer: The renderer
-    :param board: The board
-    """
-    for i in range(board.shape[0]):
-        for j in range(board.shape[1]):
-            p = board[i, j]
-            if p != EMPTY:
-                r = i + 1
-                c = j + 1
-                tile = rendering.Image(ASSETS.get(p), SQUARE_WIDTH, SQUARE_HEIGHT)
-                tile.set_color(1., 1., 1.)
-                tile.add_attr(rendering.Transform(translation=(c * SQUARE_WIDTH + (SQUARE_WIDTH / 2),
-                                                               (board.shape[0] - r + 1) * SQUARE_HEIGHT + (
-                                                                           SQUARE_HEIGHT / 2))))
-                viewer.add_onetime(tile)
